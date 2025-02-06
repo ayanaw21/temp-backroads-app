5860fd30905f2f164af7893717f9c8dab99814ac
Ayanaw Mengesha <ayanawmengesha21@gmail.com>
2025-02-06 04:36:26 -0800

    first commit

---
+++ src/components/Services.js
@@ -0,0 +1,34 @@
+import React from "react";
+import Title from "./Title";
+import { service } from "../data";
+const Services = () => {
+	return (
+		<section className="section services" id="services">
+			<Title title="our" subTitle="services" />
+
+			<div className="section-center services-center">
+				{service.map((item) => {
+          const {id,title,text,icon} = item;
+					return (
+						<article key={id} className="service">
+							<span className="service-icon">
+								<i className={icon}></i>
+							</span>
+							<div className="service-info">
+								<h4 className="service-title">
+									{title}
+								</h4>
+								<p className="service-text">
+									{text}
+								</p>
+							</div>
+						</article>
+					);
+				})}
+				
+			</div>
+		</section>
+	);
+};
+
+export default Services;
